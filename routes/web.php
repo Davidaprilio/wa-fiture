29e48a3eaadb61798cae6ea1cd90831e334307fe
Davidaprilio <david1.aprilio4@gmail.com>
2022-09-08 22:50:08 +0700

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,5 @@ Route::prefix('whatsapp')->name('wa')->middleware(['web'])->group(function () {
         Route::get('/{waServer:id}', [WaServerController::class, 'show'])->name('.server.show');
         Route::post('/save', [WaServerController::class, 'save'])->name('.server.save');
         Route::post('/toggle-status', [WaServerController::class, 'toggle_status'])->name('.servers.status');
-
-        return view('xample');
     });
 });
